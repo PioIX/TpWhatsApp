dad869905db1679992578e67a3329822a5d1aef2
jthames1 <jthames@pioix.edu.ar>
2025-08-28 08:14:19 -0300

    commit ingresar funcion

--- front/src/app/login/page.js
+++ front/src/app/login/page.js
@@ -2,13 +2,19 @@
 
 import Input from "../components/Input"
 import Button from "../components/Button"
+import { useRouter } from "next/router"
 
 export default function LoginPage(){
     const [email, setEmail]  = useState("")
     const [password, setPassword]  = useState("")
-    const [error, setError]  = useState("")
+    const [message, setMessage]  = useState("")
+    const router = useRouter();
 
     async function ingresar(){
+        if(!email || !password){
+            setMessage("Faltan datos")
+            return
+        }
         try{
             const res = await fetch("http://localhost:4000/login", {
                 method: "POST",
@@ -18,9 +24,33 @@ export default function LoginPage(){
                 body: JSON.stringify({email, password})
             })
             const data = await res.json()
-            
+            if(!res.ok){
+                setMessage(data.error)
+                return
+            }
+            setMessage("Ingreso exitoso")
+            router.push("/chats")
         } catch(error) {
-            setError("Error al ingresar")
+            setMessage("Error al ingresar")
+        }
+    }
+
+    async function registrarse(){
+        if(!email || !password){
+            setMessage("Faltan datos")
+            return
+        }
+        try{
+            const res = await fetch("http://localhost:4000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify({email, password})
+            })
+            const data = await res.json()
+        } catch(error) {
+            setMessage("Error al regsitrarse")
         }
     }
     
@@ -31,7 +61,7 @@ export default function LoginPage(){
                 <Input type="text" placeholder="Ingrese su mail"></Input>
                 <Input type="text" placeholder="Ingrese su contraseña"></Input>
                 <Button text="Ingresar" onClick={ingresar}></Button>
-                <Button text="Registrarse"></Button>
+                <Button text="Registrarse" onClick={registrarse}></Button>
             </div>
         </>
     )
