9cdfad3679c0ba19ccab703eb9363364235aedc5
jthames1 <jthames@pioix.edu.ar>
2025-09-01 09:59:37 -0300

    commit page chats

--- front/src/app/chats/page.js
+++ front/src/app/chats/page.js
@@ -1,5 +1,28 @@
 "use client"
 
+import { useEffect, useState } from "react"
+
 export default function ChatsPage(){
-    
+    const [chats, setChats] = useState()
+
+    useEffect(() => {
+        const fetchChats = async () => {
+            try{
+                const res = await fetch("/chats")
+                const data = await res.json()
+                setChats(data)
+                console.log("Chats traidos correctamente")
+            } catch(error) {
+                console.log("Error al traer los chats", error)
+            }
+        }
+        fetchChats()
+    }, [])
+
+    return (
+        <>
+            <h1>Chats</h1>
+
+        </>
+    )
 }
