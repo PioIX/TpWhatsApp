eb5c754a220579c64730962ecfb979581186247d
jthames1 <jthames@pioix.edu.ar>
2025-09-10 22:56:14 -0300

    commit css y boton nuevo chat

--- front/src/app/chats/page.js
+++ front/src/app/chats/page.js
@@ -2,6 +2,8 @@
 
 import Contact from "@/components/Contact";
 import { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import Popup from 'reactjs-popup';
 
 export default function ChatsPage() {
     const [chats, setChats] = useState([])
@@ -33,17 +35,52 @@ export default function ChatsPage() {
         }
     }
 
+    async function newChat(){
+        const userId= localStorage.getItem("userId")
+        const datosNewChat = {
+            userId: userId,
+        }
+        try{
+            console.log("Entro al try")
+            const response = await fetch("http://localhost:4000/newChat", {
+                method: "POST",
+                headers: { "Content-Type" : "application/json"},
+                body: JSON.stringify(datosNewChat),
+            });
+            const result = await response.json();
+            console.log("Respuesta del servidor:", result)
+            if (result.res === true) {
+                alert("¡Chat creado correctamente!");
+                setIsPopupOpen(false);
+                setEmailInput("");
+                chatsUser(userId);
+            } else {
+                console.log("Error: " + (result.message || "No se pudo crear el chat"));
+            }
+        } catch(error){
+            console.log(error);
+        }
+    }
+
     return (
         <>
-            <div>
-                <h1>Chats</h1>
-                <ul>
-                    {chats.map((chat, index) => (
-                        <li key={index}>
-                            <Contact chats={chat}></Contact>
-                        </li>
-                    ))}
-                </ul>
+            <div className={styles.container}>
+                <div className={styles.title}>
+                    <h1>Chats</h1>
+                    <button onClick={newChat}>Nuevo chat</button>
+                </div>
+                <div className={styles.chatsList}>
+                    <ul>
+                        {chats.map((chat, index) => (
+                            <li key={index}>
+                                <Contact chats={chat}></Contact>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={styles.chatContent}>
+
+                </div>
             </div>
         </>
     );
--- front/src/components/Contact.js
+++ front/src/components/Contact.js
@@ -1,19 +1,15 @@
 "use client"
 
-import { useState } from "react"
+import styles from "./Contact.module.css"
 
 export default function Contact({chats}) {
-    const [lastMsg, setLastMsg] = useState()
-    const [username, setUsername] = useState()
-
     return(
         <>
-            <button>
-                <div>
-                    <img src={chats.group_photo} alt="Profile" ></img>
-                    <div>
-                        <p className="username">{chats.chat_name}</p>
-                        <p className="lastMsg">{chats.last_message}</p>
+            <button className={styles.contactButton}>
+                <div className={styles.contactContainer}>
+                    <img src={chats.photo_group || "https://imagenes2.eltiempo.com/files/image_600_455/files/fp/uploads/2025/04/01/67ec4ef31f2ce.r_d.866-866-3464.jpeg"} alt="Profile" ></img>
+                    <div className={styles.contactInfo}>
+                        <p className={styles.username}>{chats.chat_name}</p>
                     </div>
                 </div>
             </button>
